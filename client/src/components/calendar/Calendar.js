7bcef90e5dcd4bf35f7be7a473f6d2a81a1b4729
matheoleger <71370253+matheoleger@users.noreply.github.com>
2022-05-08 16:00:37 +0200

    feat(Calendar.js):First functional calendar (we can change to another week)
    Now we can change to another week but we can't show borrowed material

--- client/src/components/calendar/Calendar.js
+++ client/src/components/calendar/Calendar.js
@@ -4,20 +4,64 @@ import { useState } from "react";
 
 export const Calendar = () => {
 
-    const getWeek = (date) => {
-        
-        return date;
+    //"Extension" de la classe "Date" récupérée sur : https://www.epochconverter.com/weeknumbers
+    Date.prototype.getWeek = function () {
+        var target  = new Date(this.valueOf());
+        var dayNr   = (this.getDay() + 6) % 7;
+        target.setDate(target.getDate() - dayNr + 3);
+        var firstThursday = target.valueOf();
+        target.setMonth(0, 1);
+        if (target.getDay() != 4) {
+            target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7);
+        }
+        return 1 + Math.ceil((firstThursday - target) / 604800000);
     }
+
+    const setCorrespondingDaysOfWeek = (date) => {
+        const dayOfMonth = date.getDate();
+        const dayOfWeek = (date.getDay() + 6) % 7;
+        // const firstDayOfWeek = new Date(date.setDate(dayOfMonth - dayOfWeek));
+        // const lastDayOfWeek = new Date(date.setDate(dayOfMonth - dayOfWeek + 6));
+        // console.log(firstDayOfWeek, lastDayOfWeek)
+        let datesOfWeek = [];
+
+        for (let i = 0; i < 7; i++) {
+            datesOfWeek[i] = new Date(date.setDate(dayOfMonth - dayOfWeek + i)).getDate();
+        }
+
+        console.log(datesOfWeek);
+        return datesOfWeek;
+    }
+
+    //Mettre une date de référence (initialisation sur le jour actuel)
+    // let referenceDate = new Date();
 
     //Mettre le mois et l'année actuelle sous le bon format (MONTH YEAR)
     const optionsForMonth = { month: 'long', year: "numeric"};
     const actualMonth = new Intl.DateTimeFormat('fr-FR', optionsForMonth).format().toUpperCase();
 
-    //Mettre l'année et les jours de la semaine
-    const actualWeek = getWeek(new Date());
+    //Mettre la semaine actuelle et le premier jour de la semaine
+    const actualWeekNumber = new Date().getWeek(); //Janvier = 0, donc Mai = 4 et pas 5
 
-    const dayOfWeek = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    const daysOfWeek = ["Lundi ", "Mardi ", "Mercredi ", "Jeudi ", "Vendredi ", "Samedi ", "Dimanche "];
+    const [referenceDate, setReferenceDate] = useState(new Date())
     const [chosenMonthAndYear, setChosenMonthAndYear] = useState(actualMonth);
+    const [chosenWeekNumber, setChosenWeekNumber] = useState(actualWeekNumber);
+    const [chosenWeekDates, setChosenWeekDates] = useState(setCorrespondingDaysOfWeek(new Date()));
+
+    setCorrespondingDaysOfWeek(new Date(2022, 4, 8));
+
+    const setNewWeek = (state) => {
+
+        let numberOfDaysToNewWeek = (state == "next") ? 7 : -7; 
+        setReferenceDate(new Date(referenceDate.setDate(referenceDate.getDate() + numberOfDaysToNewWeek)));
+        console.log(referenceDate);
+        const newMonth = new Intl.DateTimeFormat('fr-FR', optionsForMonth).format(referenceDate).toUpperCase()
+        console.log(newMonth);
+        setChosenMonthAndYear(newMonth)
+        setChosenWeekNumber(referenceDate.getWeek())
+        setChosenWeekDates(setCorrespondingDaysOfWeek(referenceDate))
+    }
 
     // const optionsForMonth = { month: 'long', year: "numeric"};
     // const actualMonth = new Intl.DateTimeFormat('fr-FR', optionsForMonth).format().toUpperCase();
@@ -27,7 +71,8 @@ export const Calendar = () => {
     // console.log(new Intl.DateTimeFormat('fr-FR', options).format());
 
     console.log(actualMonth);
-    console.log(actualWeek);
+    console.log(actualWeekNumber);
+    // console.log(firstDayOfWeek);
 
     return (
         <Box      
@@ -52,21 +97,26 @@ export const Calendar = () => {
                 <Box display="flex" alignItems="center" justifyContent="space-between">
                     <Heading as="h3" size="md" textAlign="center">{chosenMonthAndYear}</Heading>
                     <Box display="flex" flexDirection="row" alignItems="center">
-                        <Button size="xs" background="none" _hover={{ bg: 'purple' }}><ArrowLeftIcon/></Button>
-                        <Text margin={2}>Semaine ...</Text>
-                        <Button size="xs" background="none" _hover={{ bg: 'purple' }}><ArrowRightIcon/></Button>
+                        <Button size="xs" background="none" _hover={{ bg: 'purple' }} onClick={() => setNewWeek("previous")}>
+                            <ArrowLeftIcon/>
+                        </Button>
+                        <Text margin={2}>Semaine {chosenWeekNumber}</Text>
+                        <Button size="xs" background="none" _hover={{ bg: 'purple' }} onClick={() => setNewWeek("next")}>
+                            <ArrowRightIcon/>
+                        </Button>
                     </Box>
                 </Box>
                 <Table width="100%">
                     <Thead>
                         <Tr>
-                            <Th width="14%" textAlign="center" fontSize={11}>Lundi 30</Th>
-                            <Th width="14%" textAlign="center" fontSize={11}>Mardi 31</Th>
-                            <Th width="14%" textAlign="center" fontSize={11}>Mercredi 29</Th>
-                            <Th width="14%" textAlign="center" fontSize={11}>Jeudi 27</Th>
-                            <Th width="14%" textAlign="center" fontSize={11}>Vendredi 22</Th>
-                            <Th width="14%" textAlign="center" fontSize={11}>Samedi 29</Th>
-                            <Th width="14%" textAlign="center" fontSize={11}>Dimanche 30</Th>
+                            {
+                                chosenWeekDates.map((date, index) => (
+                                    <Th key={index} width="14%" textAlign="center" fontSize={11}>
+                                        {daysOfWeek[index]}
+                                        {date}
+                                    </Th>
+                                ))
+                            }
                         </Tr>
                     </Thead>
                     <Tbody height="22em">
